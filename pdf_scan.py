e42bd9fc61ec81913a76d95d12bc82d32e4a9364
k-r-a-s-s <91785539+k-r-a-s-s@users.noreply.github.com>
2022-04-28 12:38:05 +1000

    improved csv output

--- pdf_scan.py
+++ pdf_scan.py
@@ -1,8 +1,8 @@
 # scans pdf
-# use the bash script here:
+# use the bash script here (inspired by post from Kurt Pfeifle https://stackoverflow.com/a/25645224/18724266)
 # for i in *.pdf; gs -q  -o - -sDEVICE=inkcov "$i" | grep -v '^ 0.00000  0.00000  0.00000' > "$i".txt; donefor i in *.pdf; gs -q  -o - -sDEVICE=inkcov "$i" | grep -v '^ 0.00000  0.00000  0.00000' > "$i".txt
 # in the folder with pdfs to output text files containing the CMYK data in to a text file named after the name of the pdf
-# requires ghostscript
+# requires ghostscript, 
 # then this python file parses those text files, compares and spits out when it is black and white or colour on a page
 
 
@@ -12,7 +12,7 @@ import pandas as pd
 import csv
 
 
-path = '/Users/kevin/Downloads/AlisonBoastNotes2018-2019/PDFS/summaryPDFs'
+path = 'FILEPATH_TO/PDFS/here/'
 
 
 
@@ -31,11 +31,14 @@ with os.scandir(path) as it:
             blackwhite.append(df['black'].sum())
             colour.append((~df['black']).sum())
 
-
 rows = zip(filenames, filepaths, blackwhite, colour)
+headers = ('filenames', 'filepaths', 'blackwhite', 'colour')
 
-with open('outputcsv', "w") as f:
+
+
+with open('outputcsv.csv', "w") as f:
     writer = csv.writer(f)
+    writer.writerow(headers)
     for row in rows:
         writer.writerow(row)
 
